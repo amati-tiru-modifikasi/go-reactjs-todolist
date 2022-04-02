90867f7131f0e49186eba9623eebd27a0b174a59
mdestafadilah <desta.08b@gmail.com>
2022-04-02 09:25:48 +0700

    Install Fiber - install - run commandline

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import "github.com/gofiber/fiber/v2"
+
+func helloWorld(c *fiber.Ctx) error {
+	return c.SendString("hello world")
+}
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", helloWorld)
+	app.Listen(":8000")
+}
