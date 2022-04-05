6d9a0b175769fac40a4d1586f5972c4222a43e34
mdestafadilah <desta.08b@gmail.com>
2022-04-05 15:05:24 +0700

    setup gorm migration and fiber

--- main.go
+++ main.go
@@ -1,14 +1,34 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"fmt"
 
-func helloWorld(c *fiber.Ctx) error {
-	return c.SendString("hello world")
+	"github.com/amati-tiru-modifikasi/todolist/database"
+	"github.com/amati-tiru-modifikasi/todolist/models"
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func initDatabase() {
+	var err error
+	dsn := "host=localhost user=postgres password=toor dbname=todos port=5432 sslmode=disable TimeZone=Asia/Jakarta"
+	database.DBConn, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("Gagal koneksi")
+	}
+	fmt.Println("Database terkoneksi")
+	database.DBConn.AutoMigrate(&models.Todo{})
+	fmt.Println("Migrated DB")
+}
+
+func setupRoutes(app *fiber.App) {
+	app.Get("/todos", models.GetTodos)
 }
 
 func main() {
 	app := fiber.New()
-
-	app.Get("/", helloWorld)
+	initDatabase()
+	setupRoutes(app)
 	app.Listen(":8000")
 }
