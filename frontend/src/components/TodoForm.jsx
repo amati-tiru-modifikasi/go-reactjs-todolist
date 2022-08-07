5f71def92a2de5416fb9c3ef4f5a2ba62224bb42
mdestafadilah <desta.08b@gmail.com>
2022-08-08 05:56:30 +0700

    FORM PART #1

--- frontend/src/components/TodoForm.jsx
+++ frontend/src/components/TodoForm.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Form, Row, Col, Buttom, Input } from 'antd'
+import { PlusCircleFilled } from '@ant-design/icons'
+import renderEmpty from 'antd/lib/config-provider/renderEmpty';
 
-function TodoForm() {
-  return (
-    <div>TodoForm</div>
+const TodoForm = ({onFormSubmit}) => {
+  const [form] = Form.useForm();
+  const onFinish = () => {
+    onFormSubmit({
+      title: form.getFieldValue('title'),
+      completed: false
+    })
+    console.log(form.getFieldValue('title'))
+    form.resetFields();
+  }
+  renderEmpty(
+    <Form>
+      
+    </Form>
   )
 }
 
