475c6f7d2e73c2b5380a7afefb7680f5aae0d35e
mdestafadilah <desta.08b@gmail.com>
2022-08-08 06:27:08 +0700

    TODO PART #2

--- frontend/src/components/TodoItem.jsx
+++ frontend/src/components/TodoItem.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Tooltip, Tag, List, Button, PopConfirm, Switch } from 'antd'
 import { CloseOutlined, CheckOutlined } from '@ant-design/icons'
 
@@ -6,7 +6,28 @@ const TodoItem = ({ todo, onTodoRemoval, onTodoToggle }) => {
   return (
     <List.Item
       actions={[
-
+        <Tooltip
+          title={todo.completed ? 'Mark as uncomplted' : 'Mark as Completed'}
+        >
+          <Switch 
+            checkedChildren={<CheckOutlined />}
+            unCheckedChildren={<CloseOutlined />}
+            onChange={() => onTodoToggle(todo)}
+            defaultChecked={todo.completed}
+          />
+        </Tooltip>,
+        <PopConfirm
+          title={'Are you sure want to delete? '}
+          onConfirm={() => {
+            onTodoRemoval(todo);
+          }}
+          className="list-item"
+          key={todo.id}
+        >
+          <Button className='remove-todo-button' type="primary" danger>
+            x
+          </Button>
+        </PopConfirm>
       ]}
     >
       <div className='todo-item'>
