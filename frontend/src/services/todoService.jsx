1e4d55b1cf05bfdcb4f6416cc7e4f76f3ca876ff
mdestafadilah <desta.08b@gmail.com>
2022-08-08 05:51:18 +0700

    SERVICE PART #2

--- frontend/src/services/todoService.jsx
+++ frontend/src/services/todoService.jsx
@@ -10,3 +10,38 @@ export const getTodo = async (id) => {
   return await res.json();
 }
 
+// INSERT
+export const createTodo = (todo) => {
+  return fetch(baseUrl, {
+    method:"POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      title: todo.title,
+      completed: todo.completed
+    })
+  }).then((res) => res.json())
+}
+
+// UPDATE
+export const updateTodo = (todo) => {
+  return fetch(`${baseUrl}/${todo.id}`, {
+    method:"PUT",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      id: todo.id,
+      title: todo.title,
+      completed: todo.completed
+    })
+  }).then((res) => res.json())
+}
+
+// DELETE
+export const deleteTodo = (id) => {
+  return fetch(`${baseUrl}/${id}`, {
+    method: "DELETE"
+  }).then(res => res.json())
+}
