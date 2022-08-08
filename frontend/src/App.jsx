cbc93cef630ec2510e5e579b5f557e688a43562f
mdestafadilah <desta.08b@gmail.com>
2022-08-08 12:01:29 +0700

    DONE!!!

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import TodoList from './components/TodoList';
+import "antd/dist/antd.css";
 
 const App = () => (
   <div className="App">
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,6 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from './App'
-import "./assets/css/todolist.css";
 
-const container = document.getElementById('root');
-const root = createRoot(container); // createRoot(container!) if you use TypeScript
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App/>);
