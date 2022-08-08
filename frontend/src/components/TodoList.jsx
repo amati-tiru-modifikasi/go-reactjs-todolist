9f0a5b7109fca80ae6d08053d1e426e99caf29eb
mdestafadilah <desta.08b@gmail.com>
2022-08-08 10:07:33 +0700

    TODO STATE

--- frontend/src/components/TodoList.jsx
+++ frontend/src/components/TodoList.jsx
@@ -1,6 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState, useCallback } from 'react'
+import { Tabs, Layout, Row, Col, Input, message } from 'antd'
+import '../assets/css/TodoList.css'
+import TodoTab from './TodoTab'
+import TodoForm from './TodoForm'
+import { createTodo, deleteTodo, loadTodos, updateTodo } from '../services/todoService'
 
-function TodoList() {
+const { TabPane } = Tabs;
+const { Content } = Layout;
+
+const TodoList = () => {
+
+  const [refreshing, setRefreshing] = useState(false);
+  const [todos, setTodos] = useState([]);
+  const [activeTodos, setActiveTodos] = useState([]);
+  const [completedTodos, setCompletedTodos] = useState();
+
+  const handleFormSubmit = (todo) => {
+    console.log(todo)
+    createTodo(todo).then(onRefresh())
+    message.success('Todo added!')
+  }
+
+  const handleRemoveTodo = (todo) => {
+    deleteTodo(todo.id).then(onRefresh())
+    message.warn('Todo removed!')
+  }
+
+  const handleToggleTodoStatus = (todo) => {
+    todo.completed = !todo.completed;
+    updateTodo(todo).then(onRefresh());
+    message.info('Todo status updated!')
+  }
+  
   return (
     <div>TodoList</div>
   )
